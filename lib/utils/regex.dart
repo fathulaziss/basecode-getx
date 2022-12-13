5b577b1a154169ec4c64fd5176cfc8e21670f8e6
Muhamad Fathul Azis <azisfathl@gmail.com>
2022-12-13 16:56:04 +0700

    update

--- lib/utils/regex.dart
+++ lib/utils/regex.dart
@@ -20,3 +20,20 @@ bool isValidPassword({required String password, int minLength = 6}) {
   // &&
   // hasSpecialCharacters;
 }
+
+String convertCapitalCase(String text) {
+  final newText =
+      text.isNotEmpty ? '${text[0].toUpperCase()}${text.substring(1)}' : '';
+  // ex : Hello world
+  return newText;
+}
+
+String convertTitleCase(String text) {
+  final newText = text
+      .replaceAll(RegExp(' +'), ' ')
+      .split(' ')
+      .map((str) => convertCapitalCase(str))
+      .join(' ');
+  // ex : Hello World
+  return newText;
+}
